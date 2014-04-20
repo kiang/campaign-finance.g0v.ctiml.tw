8c7bb168798ee88765711dbbde1c30a78ec2442c
kiang <kiange@gmail.com>
2014-04-21 03:05:59 +0800

    proof of concept to load large image instead of cells

--- static/js/cell.js
+++ static/js/cell.js
@@ -1,82 +1,136 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var submitAnswer = function(e) {
-    if (e !== undefined) {
-      e.preventDefault();
+    var submitAnswer = function(e) {
+        getCellImage();
+        return;
+        if (e !== undefined) {
+            e.preventDefault();
+        }
+
+        var ans = $('#ans').val();
+
+        if ($(this).hasClass("confirm")) {
+            ans = $('.cell-info').data('ans');
+        }
+
+        if ($(this).hasClass("quick-answer")) {
+            ans = $(this).data('answer');
+        }
+
+        if (ans === "" && $(this).hasClass("no-content") === false && $(this).hasClass("confirm") === false) {
+            return;
+        }
+
+        var page = $('.cell-info').data('page');
+        var x = $('.cell-info').data('x');
+        var y = $('.cell-info').data('y');
+
+        $('#submit,#no-content').attr('disabled', 'disabled');
+
+        var url = ['/api/fillcell/', page, "/", x, "/", y].join("");
+        $.post(url, {ans: ans}, function(res) {
+            getCellImage();
+            $('#submit,#no-content').removeAttr('disabled');
+        });
+    };
+
+    var img_data;
+    var currentCell = {
+        row: 0,
+        col: 0
+    };
+    var currentPage = {};
+
+    var getRandomPage = function() {
+        $.get('page.json', function(res) {
+            currentPage = res;
+            currentPage.id = 2579;
+            currentPage.lineCount = 0;
+            for (x in res.data.tables) {
+                ++currentPage.lineCount;
+            }
+            getCellImage();
+        });
     }
 
-    var ans = $('#ans').val();
+    var getCellImage = function() {
+        $('#ans').val("").focus();
+        $('.cell-info').text("圖片載入中...");
+        $('.confirm').hide();
 
-    if ($(this).hasClass("confirm")) {
-      ans = $('.cell-info').data('ans');
+        var lefttop = currentPage.data.tables[currentCell.row][currentCell.col]['left_top'];
+        var rightdown = currentPage.data.tables[currentCell.row][currentCell.col]['right_down'];
+        var source_width = parseInt(rightdown[0] - lefttop[0]);
+        var source_height = parseInt(rightdown[1] - lefttop[1]);
+        var target_width = (source_width > source_height) ? 400 : Math.floor(400 * source_width / source_height);
+        var target_height = (source_height > source_width) ? 400 : Math.floor(400 * source_height / source_width);
+
+        img_data = new Image;
+        img_data.src = currentPage.data.meta.pic_url;
+        img_data.onload = function() {
+            $('#canvas')[0].getContext('2d').clearRect(0, 0, 400, 400);
+            $('#canvas')[0].getContext('2d').drawImage(
+                    img_data,
+                    lefttop[0], // source_x
+                    lefttop[1], // source_y
+                    source_width, // source_width
+                    source_height, // source_height
+                    0, // target_x
+                    0, // target_y
+                    target_width,
+                    target_height);
+        };
+
+        $('.cell-info').data({
+            page: currentPage.id,
+            x: currentCell.row,
+            y: currentCell.col,
+            ans: ''
+        })
+                .text("")
+                .append($('<span></span>').text("第 " + currentPage.id + " 頁 (" + currentCell.row + ", " + currentCell.col + " )"));
+
+//        if (res.ans !== null) {
+//            $('.cell-info').append($('<span></span>').text(" 已經有" + res.count + "人填寫確認了，目前答案：").append($('<code></code>').text(res.ans)));
+//            $('.confirm').show();
+//        }
+
+        ++currentCell.row;
+        if (currentCell.row > currentPage.lineCount - 1) {
+            currentCell.row = 0;
+            ++currentCell.col;
+            if (currentCell.col > 8) {
+                currentCell.row = 0;
+                currentCell.col = 0;
+                getRandomPage();
+            }
+        }
+    };
+    if ('undefined' === typeof (currentPage.data)) {
+        getRandomPage();
+    } else {
+        getCellImage();
     }
 
-    if ($(this).hasClass("quick-answer")) {
-        ans = $(this).data('answer');
-    }
-
-    if (ans === "" && $(this).hasClass("no-content") === false && $(this).hasClass("confirm") === false) {
-      return;
-    }
-
-    var page = $('.cell-info').data('page');
-    var x = $('.cell-info').data('x');
-    var y = $('.cell-info').data('y');
-
-    $('#submit,#no-content').attr('disabled', 'disabled');
-
-    var url = ['/api/fillcell/', page, "/", x, "/", y].join("");
-    $.post(url, { ans: ans }, function(res){
-      getRandomImage();
-      $('#submit,#no-content').removeAttr('disabled');
+    $('#submit').click(submitAnswer);
+    $('#no-content').click(submitAnswer);
+    $('#confirm').click(submitAnswer);
+    $('.quick-answer').click(submitAnswer);
+    $('#quick-trigger').click(function() {
+        $('.quick-answer').toggle();
+        $('.open-close').text($('.quick-answer').is(':visible') ? "關閉" : "開啟");
     });
-  };
 
-  var getRandomImage = function() {
-    $('#ans').val("").focus();
-    $('.cell-info').text("圖片載入中...");
-    $('.confirm').hide();
-    $('.cell-image').html("");
-    
-    $.get('/api/getrandom', function(res) {
-      $('.cell-image').html($('<img></img>').attr('src', res.img_url).bind('error', function(){ getRandomImage(); }));
-      $('.cell-info').data({
-        page: res.page,
-        x: res.x,
-        y: res.y,
-        ans: res.ans
-      })
-      .text("")
-      .append($('<span></span>').text("第 "+res.page+" 頁 ("+res.x+", "+res.y+" )"));
+    $('#next').click(getCellImage);
 
-      if (res.ans !== null) {
-        $('.cell-info').append($('<span></span>').text(" 已經有" +res.count + "人填寫確認了，目前答案：").append($('<code></code>').text(res.ans)));
-        $('.confirm').show();
-      }
-
+    $('#ans').keypress(function(e) {
+        if (e.which == 13) {
+            if (e.shiftKey) {
+                submitAnswer.apply($("#no-content")[0]);
+            }
+            e.preventDefault();
+            submitAnswer();
+        }
     });
-  };
-  getRandomImage();
-
-  $('#submit').click(submitAnswer);
-  $('#no-content').click(submitAnswer);
-  $('#confirm').click(submitAnswer);
-  $('.quick-answer').click(submitAnswer);
-  $('#quick-trigger').click(function(){
-    $('.quick-answer').toggle();
-    $('.open-close').text($('.quick-answer').is(':visible') ? "關閉" : "開啟");
-  });
-
-  $('#next').click(getRandomImage);
-
-  $('#ans').keypress(function(e) {
-    if (e.which == 13) {
-      if (e.shiftKey) {
-        submitAnswer.apply($("#no-content")[0]);
-      }
-      e.preventDefault();
-      submitAnswer();
-    }
-  });
 
 });
